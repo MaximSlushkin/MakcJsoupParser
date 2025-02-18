795a00d4a649c8e0f269cc9d06094006e250924a
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-02-18 17:18:27 +0300

    KS-5-8 task is solved

--- src/main/kotlin/lesson5_task8.kt
+++ src/main/kotlin/lesson5_task8.kt
@@ -1,2 +1,18 @@
 package org.example
 
+import org.jsoup.Jsoup
+
+fun main() {
+
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+
+    val doc = Jsoup.connect(url).get()
+
+    val citations = doc.select("#__next > div > section > div.sc-1vlzmga-2.dzxAnh > div:nth-child(1)")
+
+    for (citation in citations) {
+        println(citation.text())
+
+    }
+
+}
